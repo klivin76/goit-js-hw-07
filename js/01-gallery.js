ee820da4a4c45c4d52732c2cb08c29363acae3b3
Klementiev Ivan <klivin76@gmail.com>
2023-02-10 23:32:45 +0200

    Task 1

---
+++ js/01-gallery.js
@@ -0,0 +1,37 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+const gallery = document.querySelector(`.gallery`);
+const itemsMarkup = createGalleryItemsMarkup(galleryItems);
+gallery.insertAdjacentHTML("beforeend", itemsMarkup);
+
+function createGalleryItemsMarkup(galleryItems) {
+    return galleryItems.map(
+    ({ preview, original, description }) => {
+        return `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+        <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+        />
+    </a>    
+</div>`;
+}).join(``);        
+}
+gallery.addEventListener(`click`, onGalleryContainerClick);
+function onGalleryContainerClick(e) {
+    e.preventDefault();
+    if (!e.target.dataset.source)
+        return;
+      
+    const instance = basicLightbox.create(`
+    <img src="${e.target.dataset.source}" width="800" height="600">
+`)
+
+instance.show()
+    } 
+ 
+
+
